9805b40b01826b0d960b9bf8d81cfbef21d4a408
Austin Wright <aaa@bzfx.net>
2015-12-23 18:31:48 -0700

    Maybe fix broadcast packets?

--- httpd.js
+++ httpd.js
@@ -21,69 +21,40 @@ var sock0 = device.sock0 = dgram.createSocket("udp4");
 var sock1 = device.sock1 = dgram.createSocket("udp4");
 var sock2 = device.sock2 = dgram.createSocket("udp4");
 
-
 sock0.on("message", device.onMsg0.bind(device));
 sock1.on("message", device.onMsg1.bind(device));
 sock2.on("message", device.onMsg2.bind(device));
 
 sock0.on("listening", function () {
 	var address = sock0.address();
-	sock0.addMembership(device.broadcastIP, device.ipaddr);
 	console.log("server listening " +	address.address + ":" + address.port);
 });
 sock1.on("listening", function () {
 	var address = sock1.address();
-	sock1.addMembership(device.broadcastIP, device.ipaddr);
 	console.log("server listening " +	address.address + ":" + address.port);
 });
 sock2.on("listening", function () {
 	var address = sock2.address();
-	sock2.addMembership(device.broadcastIP, device.ipaddr);
 	console.log("server listening " +	address.address + ":" + address.port);
 });
 
-net.createServer(function(socket) {
-	console.log('NEW CONNECTION 12523', socket);
-	socket.on('end', function() {
-		socket.end('I got your message (but didnt read it)\n');
-	});
-	// start the flow of data, discarding it.
-	socket.resume();
-}).listen(12523);
-
-
-net.createServer(function(socket) {
-	console.log('NEW CONNECTION 1053', socket);
-	socket.on('end', function() {
-		socket.end('I got your message (but didnt read it)\n');
-	});
-	// start the flow of data, discarding it.
-	socket.resume();
-}).listen(1053);
-
-net.createServer(function(socket) {
-	console.log('NEW CONNECTION 1051', socket);
-	socket.on('end', function() {
-		socket.end('I got your message (but didnt read it)\n');
-	});
-	// start the flow of data, discarding it.
-	socket.resume();
-}).listen(1051);
-
 var waiting = 3;
 sock0.bind(50000, device.ipaddr, function onBound0(){
-	console.log('bound0');
+	console.log('bound0', device.ipaddr, device.broadcastIP);
 	sock0.setBroadcast(true);
+	sock0.addMembership(device.broadcastIP);
 	doneBind();
 });
 sock1.bind(50001, device.ipaddr, function onBound1(){
-	console.log('bound1');
+	console.log('bound1', device.ipaddr, device.broadcastIP);
 	sock1.setBroadcast(true);
+	sock1.addMembership(device.broadcastIP);
 	doneBind();
 });
 sock2.bind(50002, device.ipaddr, function onBound2(){
-	console.log('bound2');
+	console.log('bound2', device.ipaddr, device.broadcastIP);
 	sock2.setBroadcast(true);
+	sock2.addMembership(device.broadcastIP);
 	doneBind();
 });
 function doneBind(){
