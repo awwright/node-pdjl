874052be093136030c436c66775406a037bf5bd9
Austin Wright <aaa@bzfx.net>
2016-07-26 23:10:47 -0700

    SMPTE file makes for a lousy example

--- smpte.js
+++
@@ -1,6 +0,0 @@
-
-var smpte = require('./smptedata.js');
-var Speaker = require('speaker');
-var smpteEncode = new smpte.smpteEncode();
-var speaker = new Speaker(smpteEncode.speakerOpts());
-smpteEncode.pipe(speaker);
